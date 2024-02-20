6a34a336c89ec8ca62d256e2483603978ca1ab4b
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2024-02-21 00:53:03 +0530

    feat: Auth and Sidebar

---
+++ components/shared/mobile-sidebar.tsx
@@ -0,0 +1,27 @@
+import { Menu } from "lucide-react";
+import React from "react";
+import { Button } from "../ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Sidebar from "./Sidebar";
+
+export default function MobileSidebar() {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side={"left"} className="p-0">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+}
