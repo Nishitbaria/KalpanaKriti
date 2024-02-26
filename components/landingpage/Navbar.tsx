9f3b0689c8cfe2f9bf66225c5947fcd5bbc93bd2
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2024-02-27 00:40:15 +0530

    Update database URL and navbar links

--- components/landingpage/Navbar.tsx
+++ components/landingpage/Navbar.tsx
@@ -75,24 +75,7 @@ export const Navbar = () => {
           </div>
 
           {/* TOP RIGHT CORNER */}
-          <div className="hidden lg:flex items-center justify-between gap-4">
-            <SignedOut>
-              <button
-                className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg text-gray-900 hover:bg-gray-900/10 active:bg-gray-900/20"
-                type="button"
-                data-ripple-light="true"
-              >
-                Log in
-              </button>
-              <button
-                className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
-                type="button"
-                data-ripple-light="true"
-              >
-                buy now
-              </button>
-            </SignedOut>
-          </div>
+          <div className="hidden lg:flex items-center justify-between gap-4"></div>
 
           <div className="flex items-center justify-between space-x-2">
             {/* Dark mode Light Mode Button */}
@@ -119,6 +102,14 @@ export const Navbar = () => {
                 </DropdownMenuItem>
               </DropdownMenuContent>
             </DropdownMenu>
+            <SignedOut>
+              <Link
+                href="/login"
+                className="align-middle  lg:block hidden select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg text-gray-900 hover:bg-gray-900/10 active:bg-gray-900/20"
+              >
+                Log in
+              </Link>
+            </SignedOut>
             <UserButton />
             <MobileSidebar2 />
           </div>
--- components/shared/mobile-sidebar.tsx
+++ components/shared/mobile-sidebar.tsx
@@ -66,8 +66,18 @@ export const sidebarLinks: SidebarLink[] = [
   },
   {
     imgURL: "/logo.png",
-    route: "/community",
-    label: "Community",
+    route: "/Price",
+    label: "Price",
+  },
+  {
+    imgURL: "/logo.png",
+    route: "/FAQ",
+    label: "FAQs",
+  },
+  {
+    imgURL: "/logo.png",
+    route: "/dashboard",
+    label: "Dashboard",
   },
 ];
 
@@ -75,7 +85,7 @@ const NavContent = () => {
   const pathname = usePathname();
 
   return (
-    <section className="flex h-full flex-col gap-6 pt-16">
+    <section className="flex h-full flex-col gap-2 pt-16">
       {sidebarLinks.map((item) => {
         const isActive =
           (pathname.includes(item.route) && item.route.length > 1) ||
@@ -87,7 +97,7 @@ const NavContent = () => {
               href={item.route}
               className={`${
                 isActive
-                  ? "primary-gradient rounded-lg text-light-900"
+                  ? "bg-gray-300 rounded-lg text-light-900"
                   : "text-dark300_light900"
               } flex items-center justify-start gap-4 bg-transparent p-4`}
             >
@@ -131,15 +141,10 @@ export function MobileSidebar2() {
         className="background-light900_dark200 border-none"
       >
         <Link href="/" className="flex items-center gap-1">
-          <Image
-            src="/assets/images/site-logo.svg"
-            width={23}
-            height={23}
-            alt="DevFlow"
-          />
+          <Image src="/logo.png" width={23} height={23} alt="DevFlow" />
 
           <p className="h2-bold text-dark100_light900 font-spaceGrotesk">
-            Dev <span className="text-primary-500">Overflow</span>
+            KalpanaKriti
           </p>
         </Link>
         <div>
@@ -151,7 +156,7 @@ export function MobileSidebar2() {
             <div className="flex flex-col gap-3">
               <SheetClose asChild>
                 <Link href="/sign-in">
-                  <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                  <Button className="small-medium light-border-2 bg-sky-400  min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none">
                     <span className="primary-text-gradient">Log In</span>
                   </Button>
                 </Link>
@@ -159,7 +164,7 @@ export function MobileSidebar2() {
 
               <SheetClose asChild>
                 <Link href="/sign-up">
-                  <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none">
+                  <Button className="small-medium light-border-2 bg-sky-400  min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none">
                     Sign Up
                   </Button>
                 </Link>
